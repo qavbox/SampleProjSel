339a0feb86894287991ae2af1fa7921d9bc2660b
qavbox <qavbox@gmail.com>
2022-12-30 13:21:56 +0530

    tc updated

--- src/test/java/pom/tc/LoginGetItemsTest.java
+++ src/test/java/pom/tc/LoginGetItemsTest.java
@@ -27,7 +27,7 @@ public class LoginGetItemsTest extends BaseTest {
 
         Thread.sleep(2000);
 
-        Assert.assertEquals(prodPage.getItemsSize(), 5);
+        Assert.assertEquals(prodPage.getItemsSize(), 6);
 
         System.out.println("prod count 6");
 
